44d17cb1bd93185772c2fa57cb98207e39910c51
ChristopherSalam <christopher.salam@gmail.com>
2015-06-11 15:18:04 -0700

    Worked on getElementsByClassName from scratch.
    What a difficult problem! It took quite a while to even understand what
    the question was asking. After a bit of digging, it became clear that
    the problem was to traverse through each DOM element and check to see
    if it had the class attribute we were looking for. I never thought
    about the HTML/JS behind the scenes. My final code created an array,
    looked through each DOM element, scanned for class attributes and
    recursed the formula when DOM elements existed but the targets weren't
    found until the entire tree had been looked through. I consoled log
    each of the test results to help me figure out that I had passed 6 out
    of 7 tests, my formula at one point wasn't going deep enough, and it
    was because of a error in logic (node.childNodes[i].childNodes vers
    node.childNodes.childNodes.length) in my recursive statement. Very
    cool, taking a break.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className){
+	//console.log(document.body.getAttribute("class"));
+	var elements = document.body;
+	var targets = [];
+	    if(elements.getAttribute && elements.getAttribute('class')){
+    		if(elements.getAttribute('class').split(" ").indexOf(className) >= 0){
+    			targets.push(elements);
+    		}
+    	}
+	function lookup(node){
+    	for (var i=0;i<node.childNodes.length;i++){
+    		//console.log(node.childNodes[i].childNodes);
+    		if (node.childNodes[i].childNodes.length > 0){ lookup(node.childNodes[i]); } //keep digging 
+    		if(node.childNodes[i].getAttribute && node.childNodes[i].getAttribute('class')){
+    			//console.log(node.childNodes[i].getAttribute('class').split(" "));
+    			if(node.childNodes[i].getAttribute('class').split(" ").indexOf(className) >= 0){
+    				targets.push(node.childNodes[i]);
+    			}
+    		}
+		}
+	}
+	lookup(elements);
+    return targets;
 };
