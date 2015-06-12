232f6ca2f1d69d179ba8843b2fb2d8119c84a1c0
ChristopherSalam <christopher.salam@gmail.com>
2015-06-12 15:37:22 -0700

    JSON.parse and JSON.stringify built, from scratch
    JSON.parse came together quick, because I understand that the often
    disparaged eval function did something pretty close to what I wanted,
    i.e. looking at a string and making it into JS. The stringily was
    straightforward but because of escape characters and the various
    different data types in JS it took a while to make this but it looks
    good. As before, console logging out the result and the expected really
    helped me see the gaps between where I was and where I wanted to go.

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -4,13 +4,18 @@ describe('stringifyJSON', function(){
 
     stringifiableObjects.forEach(function(test){
       var expected = JSON.stringify(test);
+      //console.log(expected);
       var result = stringifyJSON(test);
+      //console.log(result);      
       expect(result).to.equal(expected);
+      //console.log(expect(result).to.equal(expected));
     });
 
     unstringifiableValues.forEach(function(obj){
       var expected = JSON.stringify(obj);
+      //console.log(expected);
       var result = stringifyJSON(obj);
+      //console.log(result); 
       expect(result).to.equal(expected);
     });
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function(value) {
   // your code goes here
-};
+      var escMap = {'"': '\\"', '\\': '\\\\', '\b': '\\b', '\f': '\\f', '\n': '\\n', '\r': '\\r', '\t': '\\t'};
+      var escFunc = function (m) { return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1); };
+      var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g; 
+      //From MDN polyfill for JSON parse
+      //NULL
+  	if (value == null) { 									
+  		  return 'null'; 
+      //NUMBER
+  	} else if (typeof value === 'number') {					
+          return isFinite(value) ? value.toString() : 'null';
+      //BOOLEAN
+  	} else if (typeof value === 'boolean') {
+          return value.toString();	
+      //Object & Arrays						
+  	} else if (typeof value === 'object') {
+             if (Array.isArray(value)) { //Array
+            	var res = '[';
+            	for (var i = 0; i < value.length; i++)
+              	res += (i ? ',' : '') + stringifyJSON(value[i]);
+            	return res + ']';
+          		} else if (toString.call(value) === '[object Object]') {
+            var tmp = [];
+            for (var k in value) { //Object
+              if (value.hasOwnProperty(k) && k!== "undefined" && k!== "functions"){ 
+                //This line above deals with unstringifyable sections, such as functions and undefined.
+                tmp.push(stringifyJSON(k) + ':' + stringifyJSON(value[k]));
+            	}
+        	}
+            return '{' + tmp.join(',') + '}'; 
+        }
+	};
+	return '"' + value.toString().replace(escRE, escFunc) + '"'; //To address escapes
+}
